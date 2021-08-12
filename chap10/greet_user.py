c4dad6469aa0f18454ab8680a805612f2f555589
Vladimir Lebedev <vovalebedev@bk.ru>
2021-08-12 08:57:46 +0300

    Good morning

---
+++ chap10/greet_user.py
@@ -0,0 +1,7 @@
+import json
+
+filename = "username.json"
+
+with open(filename) as file:
+    username = json.load(file)
+    print("Welcome back, " + username + '!')
---
+++ chap10/remember_me.py
@@ -0,0 +1,9 @@
+import json
+
+username = input("What is your name?")
+
+filename = "username.json"
+
+with open(filename, 'w') as file:
+    json.dump(username, file)
+    print("We'll remember you when you come back, " + username + '!')
