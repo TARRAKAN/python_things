128cc09699f30701028b05accb16769e8f62dbfb
Vladimir Lebedev <vovalebedev@bk.ru>
2021-08-16 08:37:21 +0300

    bullets added

--- Python Crash Course/Alien Invasion/alien_invasion.py
+++ Python Crash Course/Alien Invasion/alien_invasion.py
@@ -3,14 +3,21 @@ import pygame
 import game_functions
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
+
+
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
+    bullets = Group()
+
     while(True):
-        game_functions.check_events(ship) 
+        game_functions.check_events(ai_settings, screen, ship, bullets)##
+        #game_functions.check_events(ship) 
         ship.update()
-        game_functions.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        game_functions.update_screen(ai_settings, screen, ship, bullets)
 run_game()
