5040373f4847fac9c0e8770ce4fc31c93995517b
Vladimir Lebedev <vovalebedev@bk.ru>
2021-08-19 14:23:28 +0300

    game's over added

--- Python Crash Course/Alien Invasion/alien_invasion.py
+++ Python Crash Course/Alien Invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import game_functions
 from settings import Settings
 from ship import Ship
 from alien import Alien
+from game_stats import GameStats
 from pygame.sprite import Group
 
 
@@ -12,19 +13,19 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    stats = GameStats(ai_settings)
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
     game_functions.create_fleet(ai_settings, screen, ship, aliens)
 
     while(True):
-        game_functions.check_events(ai_settings, screen, ship, bullets)##
-        #game_functions.check_events(ship) 
-        ship.update()
-        bullets.update()
-        game_functions.update_bullets(aliens, bullets)
-        game_functions.update_aliens(ai_settings, aliens)
-        game_functions.update_screen(ai_settings, screen, ship, aliens, bullets)
+        game_functions.check_events(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            game_functions.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            game_functions.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            game_functions.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
