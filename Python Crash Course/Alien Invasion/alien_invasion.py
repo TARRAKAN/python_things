0f4cb52a7754b968330b535ad370357636ea6738
Vladimir Lebedev <vovalebedev@bk.ru>
2021-08-20 10:06:11 +0300

    play button added

--- Python Crash Course/Alien Invasion/alien_invasion.py
+++ Python Crash Course/Alien Invasion/alien_invasion.py
@@ -6,13 +6,14 @@ from ship import Ship
 from alien import Alien
 from game_stats import GameStats
 from pygame.sprite import Group
-
+from button import Button
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
     stats = GameStats(ai_settings)
     ship = Ship(ai_settings, screen)
     bullets = Group()
@@ -20,12 +21,13 @@ def run_game():
     game_functions.create_fleet(ai_settings, screen, ship, aliens)
 
     while(True):
-        game_functions.check_events(ai_settings, screen, ship, bullets)
+        game_functions.check_events(ai_settings, screen, stats, play_button, ship, bullets)
         if stats.game_active:
             ship.update()
             game_functions.update_bullets(ai_settings, screen, ship, aliens, bullets)
             game_functions.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-            game_functions.update_screen(ai_settings, screen, ship, aliens, bullets)
+        game_functions.update_screen(ai_settings, screen, stats, ship, aliens, bullets, 
+                    play_button)
 
 
 run_game()
