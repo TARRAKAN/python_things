ba60b73d04d2aff76cba944177f9f7f0ea73f8fb
Vladimir Lebedev <vovalebedev@bk.ru>
2021-08-20 11:52:13 +0300

    Mouse will be hidden when the play will be pushed

--- Python Crash Course/Alien Invasion/game_functions.py
+++ Python Crash Course/Alien Invasion/game_functions.py
@@ -35,6 +35,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        pygame.mouse.set_visible(False)
         stats.reset_stats()
         stats.game_active = True
         aliens.empty()
@@ -120,6 +121,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(1)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     check_fleet_edges(ai_settings, aliens)
