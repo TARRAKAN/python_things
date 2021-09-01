e9ce2ec6b5ac3a0b74c8260481cca5d86e4a12ba
Vladimir Lebedev <vovalebedev@bk.ru>
2021-09-01 08:15:05 +0300

    upgraded

--- Python Crash Course/Data generation/rw_visual.py
+++ Python Crash Course/Data generation/rw_visual.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
-
 from random_walk import RandomWalk
-while True:
-    rw = RandomWalk()
-    rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=3)
-    plt.show()
 
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+    plt.figure(figsize=(10,6))
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, 
+            cmap=plt.cm.Blues, edgecolor='none', s=1)
+    plt.scatter(0,0,c='green', edgecolors='none', s = 15)
+    plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red', edgecolors='none', s = 15)
+    
+    #plt.axes().get_xaxis().set_visible(False)
+    #plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
     keep_running = input("Another one?(Y/n): ")
     if keep_running == 'n':
         break
